d733864a120945137354eff52e6bdac7dd6b5370
rohitsingh <rohitsin28@gmail.com>
2024-07-31 22:58:10 +0530

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
+import Header from './componants/Header'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/sign-in' element={<SignIn/>}/>
---
+++ src/componants/Header.jsx
@@ -0,0 +1,35 @@
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
+const Header = () => {
+
+  return (
+    <header className='bg-slate-200 shadow-md'>
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to={'/'}>
+          <h1 className='font-bold text-sm sm:text-xl flex-wrap'>
+            <span className='text-slate-500'>Rohit</span>
+            <span className='text-slate-700'>Estate</span>
+          </h1>
+        </Link>
+        <form className="flex items-center bg-slate-100 p-3 rounded-lg">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder='Search...'
+            className='bg-transparent w-24 sm:w-64 outline-none'
+            aria-label="Search"
+          />
+          <FaSearch className='text-slate-600' />
+        </form>
+        <ul className='flex gap-3 text-center font-semibold capitalize'>
+          <Link to={'/'}><li className='hidden sm:inline hover:underline'>Home</li></Link>
+          <Link to={'/about'}><li className='hidden sm:inline hover:underline'>About</li></Link>
+          <Link to={'/sign-in'}><li className='hover:underline'>Sign in</li></Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
