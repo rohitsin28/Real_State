2b2cc3aed4bf3de0c78893894ed483ebcea8a9ea
rohitsingh <rohitsin28@gmail.com>
2024-07-31 22:58:32 +0530

    Create Header Component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SignIn = () => {
   return (
     <div>
-      
+      <h1>This is signIn Page</h1>
     </div>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SignUp = () => {
   return (
     <div>
-      
+      <h1>This is SignUp Page</h1>
     </div>
   )
 }
