dd0b1f6786831a8f665a28803234d7d0e77d0315
rohitsingh <rohitsin28@gmail.com>
2024-08-01 23:55:03 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 const app = express();
 
@@ -11,8 +12,9 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-
 const PORT = process.env.PORT || 5000;
+
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
