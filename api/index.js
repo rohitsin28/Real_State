2038f7cc055b5857c67d563507bb5e64acb0c375
rohitsingh <rohitsin28@gmail.com>
2024-08-01 23:33:13 +0530

    User Model

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
+
 
 const PORT = process.env.PORT || 5000;
 
