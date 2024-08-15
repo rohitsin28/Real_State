cd05ebb83fd343659739e560a17aa9f5ae1e538b
rohitsingh <rohitsin28@gmail.com>
2024-08-15 12:49:42 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,19 +1,68 @@
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useRef,useState } from 'react';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import {app} from '../firebase';
 
 const Profile = () => {
+  const fileRef = useRef(null);
   const { CurrentUser } = useSelector(state => state.user)
-  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
 
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+
+  };
   return (
     <div className='p-3 max-w-lg mx-auto my-16'>
       <h1 className='text-3xl font-semibold text-center capitalize mb-5'>profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={CurrentUser.avatar} alt="profileImg" className='rounded-full w-28 h-28 self-center mb-4'/>
-        <input type="text" placeholder='UserName' className='border p-3 rounded-lg' id='username'/>
-        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email'/>
-        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password'  />
-        <button disabled={loading} className='bg-slate-700 p-3 rounded-lg hover:opacity-80 uppercase font-semibold disabled:opacity-80 text-white'>{loading ? 'Loading...' : 'Update'}</button>
+        <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
+        <img src={formData.avatar || CurrentUser.avatar} alt="profileImg" className='rounded-full w-28 h-28 self-center mb-4 cursor-pointer' onClick={() => fileRef.current.click()} />
+        <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully uploaded!</span>
+          ) : (
+            ''
+          )}
+        </p>
+        <input type="text" placeholder='UserName' className='border p-3 rounded-lg' id='username' />
+        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' />
+        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' />
+        <button className='bg-slate-700 p-3 rounded-lg hover:opacity-80 uppercase font-semibold disabled:opacity-80 text-white'>Update</button>
       </form>
       <div className="flex justify-between p-3 text-red-600 font-semibold">
         <h1 className='hover:underline'>Delete Account</h1>
